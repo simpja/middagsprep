ed25d0ce6c0e265d6f8433b5ba82094075d56334
Simen Pjaaten <simenpjaat@gmail.com>
2023-10-20 15:12:52 +0200

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import styled from "styled-components";
 import { Recipe } from "./components/Recipe";
 import { gronnsaksbolognese } from "./recipes/gronnsaksbolognese";
+import { kyllingform } from "./recipes/kyllingform";
+import { MainPage } from "./components/MainPage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: `/${gronnsaksbolognese.name}`,
+    element: <Recipe recipe={gronnsaksbolognese} />,
+  },
+  {
+    path: "/kyllingform",
+    element: <Recipe recipe={kyllingform} />,
+  },
+]);
 
 function App() {
   return (
     <Main>
-      <Recipe recipe={gronnsaksbolognese} />
+      <RouterProvider router={router} />
     </Main>
   );
 }
